7e862b07ef95e477eb1e8844c112fb187010a4f6
SorryMakeer <97375064+SorryMakeer@users.noreply.github.com>
2022-01-15 21:53:31 +0800

    我又回来了

--- demo1/Demo1.java
+++ demo1/Demo1.java
@@ -1,5 +1,19 @@
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        int i=0;
+        String str = "abc";
+        try {
+            i = Integer.parseInt(str);
+        }catch (NumberFormatException e){
+            System.out.println("侬脑子瓦特啦！");
+        }
+
+        System.out.println(i);
+
+//        1. '0'~'9'
+//        2. +，-
+//        3. 溢出问题，int N位，-2^(N-1) ~ 2^(N-1)-1,解决方案：123:1*100+2*10+3
+//        4. Exception
     }
 }
