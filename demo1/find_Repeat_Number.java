46297ac983c4962d1d1a87becfc3c184ac6427aa
SorryMakeer <97375064+SorryMakeer@users.noreply.github.com>
2022-01-19 21:10:31 +0800

    剑指Offer 001 整数除法

---
+++ demo1/find_Repeat_Number.java
@@ -0,0 +1,21 @@
+/*
+剑指Offer 003 寻找数组中的重复数字
+
+
+*/
+public class find_Repeat_Number {
+    public int findRepeatNumber(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] == i) {
+                i++;
+                continue;
+            }
+            if(nums[nums[i]] == nums[i]) return nums[i];
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return -1;
+    }
+}
